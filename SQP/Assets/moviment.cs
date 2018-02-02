05f55337f405f686efe07db46802e65c23f21e9a
snycker <35973595+snycker@users.noreply.github.com>
2018-02-02 16:11:49 -0200

    as
    dsadasd

---
+++ SQP/Assets/moviment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class moviment : MonoBehaviour {
+
+	public Rigidbody Rb;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Rb.useGravity = true;
+	
+
+		}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, 0, 0);
+		transform.Translate(x, 0, z);
+	}
+}
